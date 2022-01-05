f99ad73cda0f49477aea0a103fcb30b05222a21d
Anthony Lei <anthonylei@pm.me>
2022-01-04 17:21:15 -0800

    Rust LSP setup

--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "sans" :size 16))
+(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -37,6 +37,10 @@
 
 ;; Project
 (setq projectile-project-search-path "~/source/")
+
+;; Rust LSP
+(after! rustic
+  (setq lsp-rust-server 'rust-analyzer))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
